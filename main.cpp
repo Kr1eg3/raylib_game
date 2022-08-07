e22105cd7360ad70aeb33173642d5fda57f667e2
Arkadiy Pozharsky <arkady.pozharsky@vicuesoft.com>
2022-08-07 18:22:25 +0400

    fixiki

--- main.cpp
+++ main.cpp
@@ -97,7 +97,6 @@ int main()
         {
             case LOGO:
             {
-                // TODO: Update LOGO screen variables here!
                 framesCounter++;    // Count frames
 
                 // Wait for 2 seconds (120 frames) before jumping to TITLE screen
@@ -108,8 +107,6 @@ int main()
             } break;
             case TITLE:
             {
-                // TODO: Update TITLE screen variables here!
-
                 // Press enter to change to GAMEPLAY screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
@@ -118,8 +115,6 @@ int main()
             } break;
             case GAMEPLAY:
             {
-                // TODO: Update GAMEPLAY screen variables here!
-
                 // Press enter to change to ENDING screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
@@ -128,7 +123,6 @@ int main()
             } break;
             case ENDING:
             {
-                // TODO: Update ENDING screen variables here!
                 // Press enter to return to TITLE screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
@@ -151,14 +145,12 @@ int main()
             {
                 case LOGO:
                 {
-                    // TODO: Draw LOGO screen here!
                     DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
                     DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
 
                 } break;
                 case TITLE:
                 {
-                    // TODO: Draw TITLE screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
                     DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
                     DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
@@ -166,7 +158,6 @@ int main()
                 } break;
                 case GAMEPLAY:
                 {
-                    // TODO: Draw GAMEPLAY screen here!
                     //BeginTextureMode(target);
                         ClearBackground(SKYBLUE);
                         BeginMode3D(camera);
@@ -192,7 +183,6 @@ int main()
                 } break;
                 case ENDING:
                 {
-                    // TODO: Draw ENDING screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
                     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
                     DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
